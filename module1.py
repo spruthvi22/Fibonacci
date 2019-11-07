03c72223118145be294df1df7445d7b95218dffd
pruthvi <s.pruthvi22@gmail.com>
2019-11-07 02:28:23 +0000

    Defining a function to return Fibonacci numbers

--- module1.py
+++ module1.py
@@ -1,13 +1,11 @@
 """
 Author: Pruthvi Suryadevara
 Email:  pruthvi.suryadevara@tifr.res.in
-Simple code to print Fibonacci numbers
+Function to return Fibonacci numbers
 
 """
-n=10
-x=[0,1]
-for i in range(n-2):
-    x=x+[x[i]+x[i+1]]
-
-for i in range(n):
-    print(x[i])
+def Fibon(n):
+    x=[0,1]
+    for i in range(n-2):
+        x=x+[x[i]+x[i+1]]
+    return(x)
---
+++ modulei1.py
@@ -0,0 +1,10 @@
+"""
+Author: Pruthvi Suryadevara
+Email:  pruthvi.suryadevara@tifr.res.in
+Simple code to call a function to print 100 Fibonacci numbers
+
+"""
+import module1 as mo
+import timeit
+
+print(mo.Fibon(100))
